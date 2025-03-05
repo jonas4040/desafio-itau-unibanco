d0ba540c47b4d7a4bfaebb049723d6f651bd9426
Jonas Felipe <jonasflp@outlook.com>
2025-03-05 16:12:56 -0300

    FEAT - clean first commit

---
+++ src/main/java/jf/estudos/desafio_itau_unibanco_2020/DesafioItauUnibanco2020Application.java
@@ -0,0 +1,13 @@
+package jf.estudos.desafio_itau_unibanco_2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauUnibanco2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauUnibanco2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/jf/estudos/desafio_itau_unibanco_2020/DesafioItauUnibanco2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package jf.estudos.desafio_itau_unibanco_2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauUnibanco2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
